b3459f45a6dcbb79cd6eb123625fe3ba83793c0b
YummyCatnip <72871364+YummyCatnip@users.noreply.github.com>
2023-10-21 14:27:41 -0300

    Added 3 Cirgon MD monsters

--- script/beta_scripts/c3934359.lua
+++ script/beta_scripts/c3934359.lua
@@ -5,4 +5,54 @@ Duel.LoadScript("yummylib.lua")
 function s.initial_effect(c)
 	-- Cannot Remove
 	aux.CannotbeRemoved(c,LOCATION_GRAVE)
+	-- Global Check
+	aux.CirgonGlobalCheck(s,c)
+	-- Search
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORIES_SEARCH)
+	e1:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCountLimit(1,id)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+end
+s.listed_names={id}
+s.listed_series={SET_CIRGON}
+-- e1 Effect Code
+function s.cfilter(c,tp)
+	return c:IsReleasable() and c:IsSetCard(SET_CIRGON) and c:IsMonster() and Duel.IsExistingMatchingCard(s.thfil,tp,LOCATION_DECK,0,1,nil,c:GetAttribute())
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_DECK,0,1,nil,tp) and Duel.GetFlagEffect(tp,3935780)==0 end
+	-- Cirgon lock
+	aux.CirgonLock(c,tp)
+	local g=Duel.Select(HINTMSG_RELEASE,false,tp,s.cfilter,tp,LOCATION_DECK,0,1,1,nil,tp)
+	Duel.Sendto(g,LOCATION_GRAVE,REASON_COST+REASON_RELEASE)
+	e:SetLabel(Duel.GetOperatedGroup():GetFirst():GetAttribute())
+end
+function s.thfil(c,att)
+	return c:IsAbleToHand() and c:IsMonster() and not c:IsCode(id) and not c:IsAttribute(att)
+end
+function s.filter2(c)
+	return c:IsSetCard(SET_CIRGON) and c:IsMonster() and c:IsAbleToHand()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_DECK,0,1,nil,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SEARCH,nil,1,tp,LOCATION_DECK)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.Select(HINTMSG_ATOHAND,false,tp,s.thfil,tp,LOCATION_DECK,0,1,1,nil,e:GetLabel()):GetFirst()
+	if tc then
+		Duel.Search(tc,tp)
+	end
 end
--- script/beta_scripts/c3934389.lua
+++ script/beta_scripts/c3934389.lua
@@ -6,7 +6,7 @@ function s.initial_effect(c)
 	-- Cannot Remove
 	aux.CannotbeRemoved(c,LOCATION_GRAVE)
 	-- Global Check
-	-- This is were I would place the auxiliary for the global check. IF I HAD ANY
+	aux.CirgonGlobalCheck(s,c)
 	-- Modulate Level
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
@@ -19,42 +19,19 @@ function s.initial_effect(c)
 	e2:SetTarget(s.target)
 	e2:SetOperation(s.operation)
 	c:RegisterEffect(e2)
-	aux.CirgonGlobalCheck(s,function()
-		local ge1=Effect.CreateEffect(c)
-		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge1:SetCode(EVENT_SPSUMMON_SUCCESS)
-		ge1:SetOperation(s.checkop)
-		Duel.RegisterEffect(ge1,0)
-		local ge2=Effect.CreateEffect(c)
-		ge2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge2:SetCode(EVENT_CHAINING)
-		ge2:SetOperation(s.checkop2)
-		Duel.RegisterEffect(ge2,0)
-	end)
 end
 s.listed_series={SET_CIRGON}
--- Global Check
-function s.cirfil(c,ep)
-	return c:IsSummonLocation(LOCATION_GRAVE) and c:IsPreviousControler(ep)
-end
-function s.checkop(e,tp,eg,ep,ev,re,r,rp)
-	if eg:IsExists(s.cirfil,1,nil,ep) then Duel.RegisterFlagEffect(ep,id,0,0,1) end
-end
-function s.checkop2(e,tp,eg,ep,ev,re,r,rp)
-	local loc=Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_LOCATION)
-	if loc==LOCATION_GRAVE then Duel.RegisterFlagEffect(ep,id,0,0,0) end
-end
 -- e1 Effect Code 
 function s.cfilter(c)
 	return c:IsReleasable() and c:IsMonster() and c:IsSetCard(SET_CIRGON)
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	if chk==0 then return not c:IsPublic() and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_DECK,0,1,nil) and Duel.GetFlagEffect(tp,id)==0 end
+	if chk==0 then return not c:IsPublic() and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_DECK,0,1,nil) and Duel.GetFlagEffect(tp,3935780)==0 end
 	-- Cirgon lock
 	aux.CirgonLock(c,tp)
 	local g=Duel.Select(HINTMSG_RELEASE,false,tp,s.cfilter,tp,LOCATION_DECK,0,1,1,nil)
-	Duel.SendtoGrave(g,REASON_COST+REASON_RELEASE)
+	Duel.Sendto(g,LOCATION_GRAVE,REASON_COST+REASON_RELEASE)
 end
 function s.filter(c)
 	return c:IsFaceup() and c:HasLevel() and c:IsSetCard(SET_CIRGON)
